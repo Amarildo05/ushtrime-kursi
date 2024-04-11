48d1533f30cb376c5519dc2db8e4c30136ab17b2
Amarildo <amarildokola36@gmail.com>
2024-04-11 02:08:48 +0200

    course ext...

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("It works!");
+
+// alert("This site is not safe");
+
+// document.write("I appear on the webpage!");
+
+// for (var i = 0; i < 100; i++) {
+//   document.write("dynamic title");
+// }
